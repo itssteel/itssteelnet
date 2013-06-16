9ee853e8e2c30d50d7070e96eedb001800deacf0
itssteel <its.tatasteel@gmail.com>
2013-06-16 17:40:08 +0530

    Update WebForm1.aspx.vb

--- SQLServer Web Application/SQLServer Web Application/WebForm1.aspx.vb
+++ SQLServer Web Application/SQLServer Web Application/WebForm1.aspx.vb
@@ -5,7 +5,7 @@ Public Class WebForm1
     Dim smsconn As New SqlConnection(ConfigurationManager.ConnectionStrings("CNS_SMSTEST").ConnectionString.ToString.Trim)
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-    'call the insert into sql table fn
+    'call the insert into sql table function
         insertIntoSMS("Pranab mukherjee", "153259", 83.4)
     End Sub
 
